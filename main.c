ea14aa321749b8ba8de79f6f3dc9736ec6a70c28
Livia Ferreira Dos Santos <liferrei@c2r11p2.42sp.org.br>
2025-11-10 19:35:11 -0300

    Livia

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 11:32:49 by liferrei          #+#    #+#             */
-/*   Updated: 2025/11/07 17:52:21 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/11/10 19:34:53 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,7 @@ int	main(int argc, char *argv[])
 {
 	t_rules	rules;
 	t_philo	*philos;
-	int		i;
 
-	i = 0;
 	if (!ft_parse_args(argc, argv, &rules))
 		return (1);
 	if (!ft_init_rules(&rules))
@@ -28,7 +26,10 @@ int	main(int argc, char *argv[])
 		return (ft_print_error(&rules));
 	rules.start_time = ft_get_time();
 	if (!ft_create_threads(&rules, philos))
+	{
 		return (ft_print_error(&rules));
+		return (1);
+	}
 	ft_join_threads(&rules, philos);
 	ft_cleanup(&rules, philos);
 	return (0);

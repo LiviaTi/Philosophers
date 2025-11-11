97ea5cf4db1a1adb2c368384e9d3da35c62dfc85
Livia Ferreira Dos Santos <liferrei@c2r8p5.42sp.org.br>
2025-11-11 19:05:40 -0300

    Livia

--- src/monitor.c
+++ src/monitor.c
@@ -6,21 +6,25 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 19:47:43 by liferrei          #+#    #+#             */
-/*   Updated: 2025/11/11 16:34:15 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/11/11 16:53:38 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
+
 static int	ft_check_death(t_philo *philo)
 {
 	long	current_time;
+	long	diff;
 
 	pthread_mutex_lock(&philo->meal_mutex);
 	current_time = ft_get_time();
-	if ((current_time - philo->last_meal) > philo->rules->time_to_die)
+	diff = current_time - philo->last_meal;
+	pthread_mutex_unlock(&philo->meal_mutex);
+
+	if (diff > philo->rules->time_to_die)
 	{
-		pthread_mutex_unlock(&philo->meal_mutex);
 		pthread_mutex_lock(&philo->rules->dead_mutex);
 		if (!philo->rules->dead)
 		{
@@ -33,35 +37,31 @@ static int	ft_check_death(t_philo *philo)
 			return (1);
 		}
 		pthread_mutex_unlock(&philo->rules->dead_mutex);
-		return (1);
 	}
-	pthread_mutex_unlock(&philo->meal_mutex);
 	return (0);
 }
 
 static int	ft_all_ate(t_rules *rules)
 {
 	int	i;
-	int	full;
 
 	if (rules->num_eat <= 0)
 		return (0);
 	i = 0;
-	full = 0;
 	while (i < rules->num_philos)
 	{
-		if (rules->philos[i].eat_count >= rules->num_eat)
-			full++;
+		if (rules->philos[i].eat_count < rules->num_eat)
+			return (0);
 		i++;
 	}
-	if (full == rules->num_philos)
-	{
-		pthread_mutex_lock(&rules->dead_mutex);
-		rules->dead = 1;
-		pthread_mutex_unlock(&rules->dead_mutex);
-		return (1);
-	}
-	return (0);
+	pthread_mutex_lock(&rules->dead_mutex);
+	rules->dead = 1;
+	pthread_mutex_unlock(&rules->dead_mutex);
+	pthread_mutex_lock(&rules->print_mutex);
+	printf("All philosophers have eaten %d times. Simulation ended.\n",
+		rules->num_eat);
+	pthread_mutex_unlock(&rules->print_mutex);
+	return (1);
 }
 
 void	*ft_monitor(void *arg)
@@ -70,7 +70,6 @@ void	*ft_monitor(void *arg)
 	int		i;
 
 	rules = (t_rules *)arg;
-	usleep(2000);
 	while (1)
 	{
 		i = 0;
@@ -82,7 +81,7 @@ void	*ft_monitor(void *arg)
 		}
 		if (ft_all_ate(rules))
 			return (NULL);
-		usleep(2000);
+		usleep(1000);
 	}
 	return (NULL);
 }
--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 15:34:35 by liferrei          #+#    #+#             */
-/*   Updated: 2025/11/11 16:31:58 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/11/11 18:57:07 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,27 +21,43 @@ static void	ft_sleep(t_philo *philo)
 static void	ft_think(t_philo *philo)
 {
 	ft_print_action(philo, "is thinking");
-	usleep(100);
+	usleep(200);
 }
 
 void	ft_eat(t_philo *philo)
 {
-	pthread_mutex_lock(philo->left_fork);
-	ft_print_action(philo, "has taken a left fork");
-	pthread_mutex_lock(philo->right_fork);
-	ft_print_action(philo, "has taken a right fork");
-
-	ft_print_action(philo, "is eating");
-
+	if (philo->rules->num_philos % 2 == 0)
+	{
+		if (philo->id % 2 == 0)
+			usleep(500);
+	}
+	else
+	{
+		if (philo->id % 2 != 0)
+			usleep(1000);
+	}
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(philo->right_fork);
+		ft_print_action(philo, "has taken a right fork");
+		pthread_mutex_lock(philo->left_fork);
+		ft_print_action(philo, "has taken a left fork");
+	}
+	else
+	{
+		pthread_mutex_lock(philo->left_fork);
+		ft_print_action(philo, "has taken a left fork");
+		pthread_mutex_lock(philo->right_fork);
+		ft_print_action(philo, "has taken a right fork");
+	}
 	pthread_mutex_lock(&philo->meal_mutex);
 	philo->last_meal = ft_get_time();
 	pthread_mutex_unlock(&philo->meal_mutex);
-
+	ft_print_action(philo, "is eating");
 	usleep(philo->rules->time_to_eat * 1000);
 	philo->eat_count++;
-
-	pthread_mutex_unlock(philo->right_fork);
 	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
 }
 
 void	*ft_routine(void *arg)
@@ -50,7 +66,8 @@ void	*ft_routine(void *arg)
 
 	philo = (t_philo *)arg;
 	if (philo->id % 2 == 0)
-		usleep(1000);
+		usleep(200);
+
 	while (1)
 	{
 		pthread_mutex_lock(&philo->rules->dead_mutex);
@@ -60,6 +77,7 @@ void	*ft_routine(void *arg)
 			break ;
 		}
 		pthread_mutex_unlock(&philo->rules->dead_mutex);
+
 		ft_eat(philo);
 		ft_sleep(philo);
 		ft_think(philo);
